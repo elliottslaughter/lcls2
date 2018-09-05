1619caf94f40dc3d272a36f2458eff0f20e9944b
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-09-05 07:43:10 -0700

    Don't do full build on Python 2.

--- run_travis.sh
+++ run_travis.sh
@@ -5,10 +5,11 @@ set -e
 source activate $CONDA_ENV
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
-./build_travis.sh -p install
 export PYTHONPATH=$PWD/install/lib/python$PYVER/site-packages
 if [[ $PYVER == 2.7 ]]; then
+    ./build_python2_psana.py
     nosetests psana/psana/tests
 elif [[ $PYVER == 3.* ]]; then
+    ./build_travis.sh -p install
     pytest psana/psana/tests
 fi
